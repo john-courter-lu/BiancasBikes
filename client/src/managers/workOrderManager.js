e0ff4ce8266476fa5fb48a7083d48a11cf0fa0e4
John Lu <johnlu2006@gmail.com>
2023-09-29 14:05:26 -0500

    add comments to the last getter in apiManager

--- client/src/managers/workOrderManager.js
+++ client/src/managers/workOrderManager.js
@@ -1,5 +1,9 @@
 const _apiUrl = "/api/workorder";
 
 export const getIncompleteWorkOrders = () => {
-  return fetch(`${_apiUrl}/incomplete`).then((res) => res.json());
+  return fetch(_apiUrl + "/incomplete").then((res) => res.json());
 };
+
+// the same:
+// fetch(_apiUrl + "/incomplete")
+// fetch(`${_apiUrl}/incomplete`)
