08dffe5110c4ac0eaec87d5939a6015c7aa547d9
John Lu <johnlu2006@gmail.com>
2023-09-29 14:19:51 -0500

    Creating the Endpoint

--- Controllers/WorkOrderController.cs
+++ Controllers/WorkOrderController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using BiancasBikes.Data;
+using BiancasBikes.Models;
 
 namespace BiancasBikes.Controllers;
 
@@ -17,6 +18,7 @@ public class WorkOrderController : ControllerBase
     }
     //the WorkOrderController constructor is injecting an instance of the BiancasBikesDbContext class to use to access the database.
 
+    // Get incomplete work orders
     [HttpGet("incomplete")]
     [Authorize]
     public IActionResult GetIncompleteWorkOrders() //first endpoint in the class with the name: GetIncompleteWorkOrder
@@ -34,5 +36,16 @@ public class WorkOrderController : ControllerBase
         //The query in the method uses OrderBy and ThenByDescending to order the work orders first by when they were created, so that the oldest appear first. Then they are further sorted by whether an employee has been assigned to them or not. If the work order does not have a UserProfileId, it will appear before one that does.
 
         //notice that we had to use Include twice for Bike. Once, to be able to call ThenInclude for Owner, and a second time to be able to call ThenInclude for BikeType.
+    }
+
+    // Post a new work order
+    [HttpPost]
+    [Authorize]
+    public IActionResult CreateWorkOrder(WorkOrder workOrder)
+    {
+        workOrder.DateInitiated = DateTime.Now;
+        _dbContext.WorkOrders.Add(workOrder);
+        _dbContext.SaveChanges();
+        return Created($"/api/workorder/{workOrder.Id}", workOrder);
     }
 }
