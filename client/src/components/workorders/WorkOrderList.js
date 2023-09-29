6fc2af099f9eeceb389d926715fafa143e7450dc
John Lu <johnlu2006@gmail.com>
2023-09-29 14:15:40 -0500

    Building the UI: add a link to the form in the list component

--- client/src/components/workorders/WorkOrderList.js
+++ client/src/components/workorders/WorkOrderList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Table } from "reactstrap";
 import { getIncompleteWorkOrders } from "../../managers/workOrderManager.js";
+import { Link } from "react-router-dom";
 
 const testWorkOrders = [
     {
@@ -83,6 +84,7 @@ export default function WorkOrderList({ loggedInUser }) {
     return (
         <>
             <h2>Open Work Orders</h2>
+            <Link to="/workorders/create">New Work Order</Link>
             <Table>
                 <thead>
                     <tr>
