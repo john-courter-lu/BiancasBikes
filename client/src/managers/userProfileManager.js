4c2f3ec26dee1efdad5cdee9a8da3eb38fb9a5f5
John Lu <johnlu2006@gmail.com>
2023-10-03 11:41:02 -0500

    add data access function for promoting and demoting

--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -4,6 +4,20 @@ export const getUserProfiles = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
 
+//get roles
 export const getUserProfilesWithRoles = () => {
     return fetch(_apiUrl + "/withroles").then((res) => res.json());
+  };
+
+//promoting and demoting
+export const promoteUser = (userId) => {
+    return fetch(`${_apiUrl}/promote/${userId}`, {
+      method: "POST",
+    });
+  };
+  
+  export const demoteUser = (userId) => {
+    return fetch(`${_apiUrl}/demote/${userId}`, {
+      method: "POST",
+    });
   };
