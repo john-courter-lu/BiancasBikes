51293059cda797be5a239e5ce9bf413b197c0df6
John Lu <johnlu2006@gmail.com>
2023-10-03 10:43:03 -0500

    add the endpoint to get UserProfiles with roles

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using BiancasBikes.Data;
+using BiancasBikes.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BiancasBikes.Controllers;
 
@@ -21,4 +23,28 @@ public class UserProfileController : ControllerBase
     {
         return Ok(_dbContext.UserProfiles.ToList());
     }
+
+    // get UserProfiles with roles
+    [HttpGet("withroles")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetWithRoles()
+    {
+        return Ok(_dbContext.UserProfiles
+        .Include(up => up.IdentityUser)
+        .Select(up => new UserProfile
+        {
+            Id = up.Id,
+            FirstName = up.FirstName,
+            LastName = up.LastName,
+            Address = up.Address,
+            Email = up.IdentityUser.Email,
+            UserName = up.IdentityUser.UserName,
+            IdentityUserId = up.IdentityUserId,
+            Roles = _dbContext.UserRoles
+            .Where(ur => ur.UserId == up.IdentityUserId)
+            .Select(ur => _dbContext.Roles.SingleOrDefault(r => r.Id == ur.RoleId).Name)
+            .ToList()
+        }));
+    }
+    // The query gets user profiles, then searches for user roles associated with the profile, and maps each of those to role names.
 }
