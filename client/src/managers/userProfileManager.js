85942b2bc9ad48a97f51a704de679c65913b8ca0
John Lu <johnlu2006@gmail.com>
2023-10-03 10:44:49 -0500

    add data access function

--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -3,3 +3,7 @@ const _apiUrl = "/api/userprofile";
 export const getUserProfiles = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
+
+export const getUserProfilesWithRoles = () => {
+    return fetch(_apiUrl + "/withroles").then((res) => res.json());
+  };
